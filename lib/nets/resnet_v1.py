ca9aaaeaec2b81b52ad8c367084a9d062bab9b3d
rluo <rluo@ttic.edu>
2017-07-30 06:17:09 -1000

    Change mean to 0.2 style.

--- lib/nets/resnet_v1.py
+++ lib/nets/resnet_v1.py
@@ -319,7 +319,7 @@ class resnetv1(Network):
     rois = self._region_proposal(net_conv)
     pool5 = self._roi_pool_layer(net_conv, rois)
 
-    fc7 = self.resnet.layer4(pool5).mean(3).squeeze(3).mean(2).squeeze(2) # average pooling after layer4
+    fc7 = self.resnet.layer4(pool5).mean(3).mean(2) # average pooling after layer4
 
     cls_prob, bbox_pred = self._region_classification(fc7)
 
